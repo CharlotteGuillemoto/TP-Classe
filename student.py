cfaf342228fcafc94d17a0f35b16c73b08896749
Charlotte Guillemoto <charlotte.guillemoto@etu.minesparis.psl.eu>
2025-11-04 15:24:21 +0100

    suite du TP

--- student.py
+++ student.py
@@ -8,14 +8,50 @@ class Student:
 
     def __repr__(self):
         return f"{self.first_name} {self.last_name}"
-    def add_grade(topic: str; grade: float)
+    def followed_topics(self):
+         return(list.grades.keys)
+
+
+    def add_grade(self, topic: str, grade: float) -> None:
+            if not ( 0 <= grade <= 20):
+                 raise ValueError("Grade must be between 0 and 20")
+            self.topic=topic
+            self.grade=grade
+            
+    
+def compute_average(self, course: str) -> float:
+        if course not in self.grades or len(self.grades[course]) == 0:
+            raise ValueError(f"No grades recorded for course: {course}")
+        return sum(self.grades[course]) / len(self.grades[course])
+    
+    def report(self):
+        """ génère un rapport formaté des moyennes par matière """
+        report_lines = []
+        header = f"Report for student {self.first_name} {self.last_name}"
+        report_lines.append(header)
+        report_lines.append("+===============+===============+")
+        report_lines.append("|     Topic     |    Average    |")
+        report_lines.append("+===============+===============+")
+        
+        for topic in self.followed_topics():
+            average = self.compute_average(topic)
+            report_lines.append(f"|  {topic:<13}|    {average:>6.2f}     |") # Format topic left-aligned in 13 spaces, average right-aligned in 6 spaces with 2 decimals
+            report_lines.append("+---------------+---------------+")
+        
+        return "\n".join(report_lines)
+
 
 try:
     student = Student("Achille", "Talon")
-    if repr(student) != "Achille Talon":
-        raise Exception("There is an issue in your __repr__ method.")
+    student.add_grade("History", 10.)
+    student.add_grade("History", 12.)
+    topics = student.followed_topics()
+    if len(topics) != 1 or "History" not in topics:
+        raise Exception(f"Expecting ['History'] got {topics}")
 except Exception as e:
-    print('OOPS - There is an issue in your code.')
+    print('OOPS - There is an issue in your followed_topics method')
     print(f"Error message : {e}")
 else:
     print('Congrats ! Your implementation works !')
+
+#print(student.followed_topics)
